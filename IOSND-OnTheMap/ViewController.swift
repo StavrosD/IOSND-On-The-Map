868f85263598b229643c276085be3a9db2c4a076
Dimopoulos Stavros tou Athanasiou <sdim@sdim.gr>
2020-01-25 23:08:08 +0200

    Initial Commit

---
+++ IOSND-OnTheMap/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IOSND-OnTheMap
+//
+//  Created by Dimopoulos Stavros tou Athanasiou on 25/1/20.
+//  Copyright © 2020 Dimopoulos Stavros tou Athanasiou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
